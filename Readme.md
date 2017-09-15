585f9c80e925443e4bf6e616caaa97a62f814bd9
PC <naruto_1218@hotmail.com>
2017-09-15 08:43:23 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+## Setup
+
+```
+$ npm install
+```
+
+## Running
+
+```
+$ npm start
+```
+
+## Build
+
+```
+$ npm run build
+```
---
+++ client/redux/constants.js
@@ -0,0 +1,14 @@
+export const INIT = '@@INIT';
+export const REDUX_INIT = '@@redux/INIT';
+
+export const DATA_SET = 'app/data/set';
+export const DATA_SET_EVENT = 'app/data/set-event';
+export const DATA_CONTACT_ADD = 'app/data/contact-add';
+export const DATA_FLIGHT_ADD = 'app/data/flight-add';
+export const DATA_GROUND_ADD = 'app/data/ground-add';
+export const DATA_HOTEL_ADD = 'app/data/hotel-add';
+export const DATA_UPDATE_OPTION = 'app/data/update-option';
+export const DATA_SAVE = 'app/data/save';
+
+export const USER_LOGIN = 'app/user/login';
+export const USER_LOGOUT = 'app/user/logout';
